9dbc1c55bfa7c4e295c08a5643e8d42f34bd213b
OneZ3ro <emilioangelo665@gmail.com>
2023-07-12 16:26:06 +0200

    add tasks and btn

---
+++ script.js
@@ -0,0 +1,25 @@
+const aggiungiTask = (submitEvent) => {
+  submitEvent.preventDefault();
+  const form = submitEvent.target;
+  const new_task_list = form.elements;
+  const lista_task = {};
+
+  for (let i = 0; i < new_task_list.length; i++) {
+    if (new_task_list[i].tagName !== "BUTTON") {
+      lista_task[new_task_list[i].id] = new_task_list[i].value;
+    }
+  }
+  console.log(lista_task);
+
+  const ul_lista = document.getElementById("ul_task_list");
+  const li = document.createElement("li");
+  const delete_btn = document.createElement("button");
+
+  li.innerText = `${lista_task.new_task}`;
+  li.classList.add("task");
+  ul_lista.appendChild(li);
+
+  delete_btn.innerText = "elimina task";
+  delete_btn.classList.add("del_btn");
+  li.appendChild(delete_btn);
+};
